669ee02108c272b28d53c86108ddb06769624243
Jolly Good <1671375+good-lly@users.noreply.github.com>
2021-03-23 19:20:23 +0100

    append styles dynamically loaded and progress

--- script.js
+++ script.js
@@ -2,31 +2,55 @@
 // GITHUB_ISSUES_LINK
 // NUM_RESULTS_PER_PAGE
 // MAX_PAGES
+// GITHUB_ACCEPTED_LABELS
+// GITHUB_ACCEPTED_CREATORS
 
 var stickyClass = 'sticky';
 var dataLabelName = 'data-label';
 var dataOrderName = 'data-order';
+var dataPageName = 'data-page';
 var dataOrderNameNewest = 'newest';
 var dataOrderNameOldest = 'oldest';
 var contentBoxClassName = 'content-box';
 var searchIdName = 'js-search';
 var searchBarName = 'js-search-bar';
-var page = 0;
+var page = 1;
+var allLabels = [];
+var allIssues = [];
 var fetchPage = function () {
     var xhr = new XMLHttpRequest();
     xhr.onload = () => {
         try {
             var payload = JSON.parse(xhr.response);
-
+            for (i = 0; i < payload.length; i++) {
+                var issue = payload[i];
+                var author = issue.user.login;
+                // check if issue authoer is accepted one
+                if (~GITHUB_ACCEPTED_CREATORS.indexOf(author)) {
+                    var newVisibleLabels = addLabelsAndGetSanitzed(
+                        issue.labels,
+                    );
+                    var newIssue = {
+                        number: issue.number,
+                        title: issue.title,
+                        body: issue.body,
+                        labels: newVisibleLabels,
+                        edited: issue.updated_at,
+                    };
+                    var found = allIssues.some(
+                        (el) => el.number === newIssue.number,
+                    );
+                    if (!found) allIssues.push(newIssue);
+                }
+            }
             if (payload.length === NUM_RESULTS_PER_PAGE && page < MAX_PAGES) {
                 page++;
                 fetchPage();
             } else {
-                console.log('all data fetched!', payload);
+                appendLabelClassesStyle();
             }
         } catch (e) {
             console.error(e);
-            // todo handle error!;
         }
     };
 
@@ -45,47 +69,38 @@ var fetchPage = function () {
     );
     xhr.send();
 };
+var addLabelsAndGetSanitzed = function (newLabels) {
+    var newVisibleLabels = [];
+    for (j = 0; j < newLabels.length; j++) {
+        var labelName = newLabels[j].name.toLowerCase();
+        var labelColor = newLabels[j].color;
+        if (labelName != GITHUB_ACCEPTED_LABELS) {
+            newVisibleLabels.push(labelName);
+            var found = allLabels.some((el) => el.labelName === labelName);
+            if (!found) allLabels.push({ labelName, labelColor });
+        }
+    }
+    return newVisibleLabels;
+};
 var ready = function () {
     var search = document.getElementById(searchIdName);
     var searchbar = document.getElementById(searchBarName);
+    var elms = document.getElementsByClassName(contentBoxClassName);
     var sticky = searchbar.offsetHeight;
-    var issuesList = (function () {
-        var elms = document.getElementsByClassName(contentBoxClassName);
-        var tempList = [];
-        tempList.push({
-            fullLabelsName: UNCATEGORIZED_LABEL,
-            labelsList: UNCATEGORIZED_LABEL,
-            order: dataOrderNameNewest,
-            labeledList: [],
-        });
-        tempList.push({
-            fullLabelsName: ALL_LABEL,
-            labelsList: ALL_LABEL,
-            order: dataOrderNameNewest,
-            labeledList: [],
-        });
-        for (i = 0; i < elms.length; i++) {
-            try {
-                var fullLabelName = elms[i].getAttribute(dataLabelName);
-                var order = elms[i].getAttribute(dataOrderName);
-                if (
-                    fullLabelName != ALL_LABEL ||
-                    fullLabelName != UNCATEGORIZED_LABEL
-                ) {
-                    tempList.push({
-                        fullLabelsName: fullLabelName,
-                        labelsList: fullLabelName.split(' '),
-                        order: order != null ? order : dataOrderNameNewest,
-                        labeledList: [],
-                    });
-                }
-            } catch (error) {
-                throw error;
-            }
+    for (k = 0; k < elms.length; k++) {
+        var id = elms[k].getAttribute('id');
+        if (id === null) {
+            var prefix = elms[k]
+                .getAttribute(dataLabelName)
+                .split(' ')
+                .join('');
+            elms[k].setAttribute('id', prefix + getRandomInt(0, 100));
         }
-        return tempList;
-    })();
-
+        var pageNum = elms[k].getAttribute(dataPageName);
+        if (pageNum === null) {
+            elms[k].setAttribute(dataPageName, 1);
+        }
+    }
     fetchPage();
 
     function stickySearch() {
@@ -98,6 +113,28 @@ var ready = function () {
     window.onscroll = function () {
         stickySearch();
     };
+};
+
+var getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var appendLabelClassesStyle = function () {
+    var tempStyle = document.createElement('style');
+    var tempStyleContent = '';
+    for (l = 0; l < allLabels.length; l++) {
+        tempStyleContent =
+            tempStyleContent +
+            ' .' +
+            allLabels[l].labelName +
+            ' { background-color: #' +
+            allLabels[l].labelColor +
+            '; }';
+    }
+    tempStyle.innerHTML = tempStyleContent;
+    document.head.appendChild(tempStyle);
 };
 
 if (
